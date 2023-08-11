ee63add038ca6ff236156c1b644159b47f81b98a
Luis Camilo Rosania Rivera <89549994+kamilo9809@users.noreply.github.com>
2023-08-11 11:11:23 -0500

    nt2 se ingresa nueva variable en proyecto

--- codigoUno.py
+++ codigoUno.py
@@ -1,3 +1,4 @@
 nombreUsuario = input("Ingresar el nombre")
 direccionCasa = input("Ingresar la Direccion")
 edad=  int(input("digite su edad"))        
+cantidaHijos = int(input("Digita el numero de hijos"))
